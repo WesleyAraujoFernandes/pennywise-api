cf48b6885dab7ba281c802d05108050cf1a26684
knowledgesolutionstecnology <knowledgesolutionstecnology@gmail.com>
2026-01-12 08:46:36 -0300

    commit inicial

---
+++ src/test/java/br/com/knowledgesoutionstecnology/pennywise/PennywiseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.knowledgesoutionstecnology.pennywise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennywiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
